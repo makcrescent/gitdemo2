24c92211c87f7dfdb4c0b21c6f8820958abffca8
aslamkhan <makcrescent@gmail.com>
2021-10-12 11:05:58 +0530

    first commit

---
+++ gitdemo2/src/main/java/com/example/gitdemo2/Gitdemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.gitdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitdemo2Application.class, args);
+	}
+
+}
---
+++ gitdemo2/src/test/java/com/example/gitdemo2/Gitdemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitdemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
